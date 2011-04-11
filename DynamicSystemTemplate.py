59192f8438997480ed0320c44eab723bb2904ccd
Jason Moore <moorepants@gmail.com>
2011-04-11 15:46:29 -0700

    added the DynamicSystem Template

---
+++ DynamicSystemTemplate.py
@@ -0,0 +1,99 @@
+class <name>():
+    """
+<description>
+
+    """
+
+    # model name
+    name = '<name>'
+
+    filename = string.join(string.split(name), "")
+    directory = 'models/' + filename + '/'
+
+    # numerical integration parameters
+<intOpts>
+    # parameter names and their values
+<parameters>
+    # state names
+<stateNames>
+    # sets the initial conditions of the states
+<initialConditions>
+    # input names
+<inputNames>
+    # output names
+<outputNames>
+    # initialize state vector
+    x = np.zeros(len(stateNames))
+
+    # initialize output vector
+    y = np.zeros(len(outputNames))
+
+    # initialize input vector
+    u = np.zeros(len(inputNames))
+
+    # initializes the zees
+<numZees>
+    # intialize the time
+    t = intOpts['ti']
+
+    def __init__(self):
+        '''Just sets the constants.'''
+        self.constants()
+
+    def constants(self):
+        '''Sets the zees that are constant.'''
+        # defines the parameters from the attribute
+        for parameter, value in self.parameters.items():
+            exec(parameter + ' = ' + str(value))
+
+<constants>
+    def f(self, x, t):
+        '''Returns the derivative of the states.'''
+
+        # defines the parameters from the attribute
+        for parameter, value in self.parameters.items():
+            exec(parameter + ' = ' + str(value))
+
+        # sets the current state
+        for i, name in enumerate(self.stateNames):
+            exec(name + ' = ' + 'x[' + str(i) + ']'))
+
+        # calculates inputs
+        u = self.inputs(t)
+        for i, name in enumerate(self.inputNames):
+            exec(name + ' = ' + 'u[' + str(i) + ']'))
+
+<eom>
+        # plug in the derivatives for returning
+        f = zeros(len(stateNames))
+        for i, name in enumerate(self.stateNames):
+            exec('f[' + str(i) + '] = ' + name + 'p')
+
+        return f
+
+    def inputs(self, t):
+        '''Returns the input value for time t.'''
+        u = np.zeros(len(self.inputNames))
+<inputs>
+        return u
+
+    def outputs(self, x):
+        '''Returns the outputs of the system with respec to the current
+        state.
+
+        '''
+        # defines the parameters locally from the attribute
+        for parameter, value in self.parameters.items():
+            exec(parameter + ' = ' + str(value))
+
+        # sets the current state
+        for i, name in enumerate(self.stateNames):
+            exec(name + ' = ' + 'x[' + str(i) + ']'))
+
+        # calculate the outputs
+<outputs>
+        # plug in the derivatives for returning
+        y = zeros(len(self.outputNames))
+        for i, name in enumerate(self.outputNames):
+            exec('y[' + str(i) + '] = ' + name + 'p')
+        return y
