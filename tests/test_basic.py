5bd2a908efb40d81a89d8b754e5ca78629a7255a
Jason Moore <moorepants@gmail.com>
2011-04-12 12:57:17 -0700

    added a test for write_list

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,3 +6,9 @@ def test_test1_al():
     alp.alparse("test1_al", "test1_al")
     assert filecmp.cmp('test1_al.txt', 'test1_al_desired.txt')
     #os.remove('test1_al.txt')
+
+def test_write_list():
+    oneLine = 'x = [1, 2, 3, 4]\n'
+    multLine = 'x = [1,\n     2,\n     3,\n     4]\n'
+    assert alp.write_list('x', ['1', '2', '3', '4'], oneLine=True) == oneLine
+    assert alp.write_list('x', ['1', '2', '3', '4']) == multLine
